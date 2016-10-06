bb0758dd19ad4352cdcc047fd2bb839fe2c920e8
Arondite <stephenknotek@gmail.com>
2016-10-06 17:56:33 -0600

    Added information to the database

--- MVC_Assignment4/Migrations/Configuration.cs
+++ MVC_Assignment4/Migrations/Configuration.cs
@@ -4,6 +4,8 @@ namespace MVC_Assignment4.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+	using MVC_Assignment4.Models;
+	using System.Collections.Generic;
 
     internal sealed class Configuration : DbMigrationsConfiguration<MVC_Assignment4.DAL.AlbumContext>
     {
@@ -14,18 +16,18 @@ namespace MVC_Assignment4.Migrations
 
         protected override void Seed(MVC_Assignment4.DAL.AlbumContext context)
         {
-            //  This method will be called after migrating to the latest version.
+			//  This method will be called after migrating to the latest version.
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
-        }
+			//  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+			//  to avoid creating duplicate seed data. E.g.
+			//
+			//    context.People.AddOrUpdate(
+			//      p => p.FullName,
+			//      new Person { FullName = "Andrew Peters" },
+			//      new Person { FullName = "Brice Lambson" },
+			//      new Person { FullName = "Rowan Miller" }
+			//    );
+			//
+		}
     }
 }
