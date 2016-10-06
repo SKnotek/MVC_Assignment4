751fdb6e44560f5305b599e8ed94628aa9ba4dc2
Arondite <stephenknotek@gmail.com>
2016-10-06 16:24:16 -0600

    Got the page to load up

---
+++ MVC_Assignment4/DAL/AlbumContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_Assignment4.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace MVC_Assignment4.DAL
+{
+	public class AlbumContext : DbContext
+	{
+		public AlbumContext() : base("AlbumContext")
+		{ }
+		public DbSet<Album> Album { get; set; }
+
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+		}
+	}
+}
---
+++ MVC_Assignment4/Models/Album.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Assignment4.Models
+{
+	public class Album
+	{
+		public int Id { get; set; }
+		public string Artist { get; set; }
+		public string Title { get; set; }
+		public DateTime ReleaseDate { get; set; }
+		public string ArtistBio { get; set; }
+	}
+}
